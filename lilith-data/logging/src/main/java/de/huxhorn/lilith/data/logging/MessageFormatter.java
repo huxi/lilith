c8f57a595870c716834846884207ce1c0a0e9b50
huxhorn <huxhorn@4afeb0b7-f550-0410-824e-ec7672375c7e>
2008-09-28 17:54:31 +0000

    Fixed typos :(
    git-svn-id: https://lilith.svn.sourceforge.net/svnroot/lilith/trunk@28 4afeb0b7-f550-0410-824e-ec7672375c7e

--- lilith-data/logging/src/main/java/de/huxhorn/lilith/data/logging/MessageFormatter.java
+++ lilith-data/logging/src/main/java/de/huxhorn/lilith/data/logging/MessageFormatter.java
@@ -20,22 +20,21 @@ package de.huxhorn.lilith.data.logging;
 import java.util.Arrays;
 
 /**
- * <p>Replacement for org.slf4j.helpers.MessageFormatter.<p/>
+ * <p>Replacement for org.slf4j.helpers.MessageFormatter.</p>
  * <p>
  * In contrast to the mentioned class, the formatting of message pattern and arguments into the actual message
  * is split into three parts:
- * <p/>
+ * </p>
  * <ol>
  * <li>Counting of placeholders in the message pattern (cheap)</li>
  * <li>Conversion of argument array into an ArgumentResult, containing the arguments converted to String as well as
  * an optional Throwable if available (relatively cheap)</li>
  * <li>Replacement of placeholders in a message pattern with arguments given as String[]. (most expensive)</li>
  * </ol>
- * <p/>
  * <p>
  * That way only the first two steps have to be done during event creation while the most expensive part, i.e. the
  * actual construction of the message, is only done on demand.
- * <p/>
+ * </p>
  */
 public class MessageFormatter
 {
