08c9a52da791e7ac6962b79dd1fa3fa2dac14598
huxhorn <huxhorn@4afeb0b7-f550-0410-824e-ec7672375c7e>
2008-10-16 08:03:15 +0000

    Ongoing work to fix the PermGenSpace problem. All TimeoutOutputStreams should be correctly closed, now.
    git-svn-id: https://lilith.svn.sourceforge.net/svnroot/lilith/trunk@39 4afeb0b7-f550-0410-824e-ec7672375c7e

--- logback/shutdown-context-listener/src/main/java/de/huxhorn/lilith/logback/servlet/LogbackShutdownServletContextListener.java
+++ logback/shutdown-context-listener/src/main/java/de/huxhorn/lilith/logback/servlet/LogbackShutdownServletContextListener.java
@@ -2,6 +2,7 @@ package de.huxhorn.lilith.logback.servlet;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContext;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.ILoggerFactory;
@@ -14,17 +15,32 @@ import ch.qos.logback.classic.LoggerContext;
 public class LogbackShutdownServletContextListener
 	implements ServletContextListener
 {
+	private ServletContext context;
+
 	public void contextDestroyed(ServletContextEvent sce)
     {
-		ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
-        if (loggerFactory instanceof LoggerContext) 
-		{
-            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-            loggerContext.shutdownAndReset();
-        }
-    }
+		shutdownLogback();
+		context=null;
+	}
 	
 	public void contextInitialized(ServletContextEvent sce)
 	{
+		if(context!=null)
+		{
+			System.err.println("There is a previous context.");
+			shutdownLogback();
+		}
+		context=sce.getServletContext();
+	}
+
+	private void shutdownLogback()
+	{
+		ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+        if (loggerFactory instanceof LoggerContext)
+		{
+            LoggerContext loggerContext = (LoggerContext) loggerFactory;
+            loggerContext.shutdownAndReset();
+			System.err.println("Logback has been shut down.");
+        }
 	}
 }
