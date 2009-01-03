cf41241628a7a452a3cd4525d028d8a3f6f9347c
Joern Huxhorn <huxhorn@users.sourceforge.net>
2009-01-03 16:30:29 +0000

    Updated to logback 0.9.14. Fixed splitpane problem. Preventing line-break in detailsView.groovy in case of extended stacktrace informations.
    git-svn-id: https://lilith.svn.sourceforge.net/svnroot/lilith/trunk@110 4afeb0b7-f550-0410-824e-ec7672375c7e

--- lilith/src/main/resources/detailsView/detailsView.groovy
+++ lilith/src/main/resources/detailsView/detailsView.groovy
@@ -1,26 +1,25 @@
+import de.huxhorn.lilith.data.access.AccessEvent
+import de.huxhorn.lilith.data.access.HttpStatus
+import de.huxhorn.lilith.data.logging.LoggingEvent
+import java.text.SimpleDateFormat
+
 /*
- * Lilith - a log event viewer.
- * Copyright (C) 2007-2008 Joern Huxhorn
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import java.text.SimpleDateFormat;
-
-import de.huxhorn.lilith.data.logging.LoggingEvent;
-import de.huxhorn.lilith.data.access.AccessEvent;
-import de.huxhorn.lilith.data.access.HttpStatus;
+* Lilith - a log event viewer.
+* Copyright (C) 2007-2008 Joern Huxhorn
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 //import de.huxhorn.lilith.data.eventsource.SourceIdentifier;
 //import de.huxhorn.lilith.data.eventsource.EventWrapper;
@@ -661,8 +660,17 @@ def buildStackTraceElement(element, ste, isFirst=false)
     {
         element.br()
     }
-    def steStr=ste.toString(true);
-    element.a([href:'ste://'+ste.toString()]) { it.mkp.yield steStr}
+    def steStr=ste.toString();
+    def extendedStr=ste.getExtendedString();
+    element.a([href:'ste://'+ste.toString()])
+            {
+                it.mkp.yield steStr
+                if(extendedStr)
+                {
+                    it.mkp.yieldUnescaped("&nbsp;")
+                    it.mkp.yield(extendedStr)
+                }
+            }
 }
 
 class EvenOddToggle
