771a5f215223d6a7b3d3ee283d1cbc88a0a4bbfa
Joern Huxhorn <huxhorn@users.sourceforge.net>
2009-01-25 02:11:32 +0000

    Added new table row "NDC" to logging table. See ticket:27. Better handling of malformed saved table layouts. Invalid column names are ignored, missing names added. Added debug action to test NDC.
    git-svn-id: https://lilith.svn.sourceforge.net/svnroot/lilith/trunk@140 4afeb0b7-f550-0410-824e-ec7672375c7e

---
+++ lilith/src/main/java/de/huxhorn/lilith/swing/table/renderer/NdcRenderer.java
@@ -0,0 +1,129 @@
+/*
+ * Lilith - a log event viewer.
+ * Copyright (C) 2007-2009 Joern Huxhorn
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.huxhorn.lilith.swing.table.renderer;
+
+import de.huxhorn.lilith.data.eventsource.EventWrapper;
+import de.huxhorn.lilith.data.logging.LoggingEvent;
+import de.huxhorn.lilith.data.logging.Message;
+import de.huxhorn.lilith.swing.table.Colors;
+import de.huxhorn.lilith.swing.table.ColorsProvider;
+
+import java.awt.*;
+import java.util.List;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+
+public class NdcRenderer
+	implements TableCellRenderer
+{
+	private LabelCellRenderer renderer;
+
+	public NdcRenderer()
+	{
+		super();
+		renderer = new LabelCellRenderer();
+		renderer.setToolTipText(null);
+		renderer.setIcon(null);
+	}
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int rowIndex, int vColIndex)
+	{
+		if(!isSelected)
+		{
+			isSelected = rowIndex == LabelCellRenderer.getSelectedRow(table);
+		}
+		if(!hasFocus && isSelected)
+		{
+			hasFocus = table.isFocusOwner();
+		}
+		renderer.setSelected(isSelected);
+		renderer.setFocused(hasFocus);
+
+		Color foreground = Color.BLACK;
+		String text = "";
+		if(value instanceof EventWrapper)
+		{
+			EventWrapper wrapper = (EventWrapper) value;
+			Object eventObj = wrapper.getEvent();
+			if(eventObj instanceof LoggingEvent)
+			{
+				LoggingEvent event = (LoggingEvent) eventObj;
+				List<Message> ndc = event.getNdc();
+
+				if(ndc != null)
+				{
+					int size = ndc.size();
+					if(size > 0)
+					{
+						Message message = ndc.get(size - 1);
+						if(message != null)
+						{
+							text = message.getMessage();
+						}
+					}
+				}
+				if(text != null)
+				{
+					int newlineIndex = text.indexOf("\n");
+					if(newlineIndex > -1)
+					{
+						int newlineCounter = 0;
+						for(int i = 0; i < text.length(); i++)
+						{
+							if(text.charAt(i) == '\n')
+							{
+								newlineCounter++;
+							}
+						}
+						text = text.substring(0, newlineIndex);
+						newlineCounter--;
+						if(newlineCounter > 0)
+						{
+							text = text + " [+" + newlineCounter + " lines]";
+						}
+					}
+				}
+			}
+		}
+		renderer.setText(text);
+
+		boolean colorsInitialized = false;
+		if(!hasFocus && !isSelected)
+		{
+			if(table instanceof ColorsProvider)
+			{
+				if(value instanceof EventWrapper)
+				{
+					EventWrapper wrapper = (EventWrapper) value;
+					ColorsProvider cp = (ColorsProvider) table;
+					Colors colors = cp.resolveColors(wrapper, rowIndex, vColIndex);
+					colorsInitialized = renderer.updateColors(colors);
+				}
+			}
+		}
+		if(!colorsInitialized)
+		{
+			renderer.setForeground(foreground);
+		}
+
+		renderer.correctRowHeight(table);
+
+		return renderer;
+	}
+}
---
+++ lilith/src/main/java/de/huxhorn/lilith/swing/table/tooltips/NdcTooltipGenerator.java
@@ -0,0 +1,73 @@
+/*
+ * Lilith - a log event viewer.
+ * Copyright (C) 2007-2009 Joern Huxhorn
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.huxhorn.lilith.swing.table.tooltips;
+
+import de.huxhorn.lilith.data.eventsource.EventWrapper;
+import de.huxhorn.lilith.data.logging.LoggingEvent;
+import de.huxhorn.lilith.data.logging.Message;
+import de.huxhorn.lilith.swing.table.TooltipGenerator;
+import de.huxhorn.sulky.formatting.SimpleXml;
+
+import java.util.List;
+
+import javax.swing.*;
+
+public class NdcTooltipGenerator
+	implements TooltipGenerator
+{
+	public String createTooltipText(JTable table, int row)
+	{
+		String tooltip = null;
+		Object value = table.getValueAt(row, 0);
+		if(value instanceof EventWrapper)
+		{
+			EventWrapper wrapper = (EventWrapper) value;
+			Object eventObj = wrapper.getEvent();
+			if(eventObj instanceof LoggingEvent)
+			{
+				LoggingEvent event = (LoggingEvent) eventObj;
+				List<Message> ndc = event.getNdc();
+				if(ndc != null && ndc.size() > 0)
+				{
+					StringBuilder buffer = new StringBuilder();
+					buffer.append("<html>");
+					appendNdc(ndc, buffer);
+					buffer.append("</html>");
+					tooltip = buffer.toString();
+				}
+			}
+		}
+		return tooltip;
+	}
+
+	public static void appendNdc(List<Message> ndc, StringBuilder buffer)
+	{
+		if(ndc != null && ndc.size() > 0)
+		{
+			buffer.append("<ul>");
+			for(Message current : ndc)
+			{
+				buffer.append("<li>");
+				buffer.append(SimpleXml.escape(current.getMessage()));
+				buffer.append("</li>");
+			}
+			buffer.append("</ul>");
+		}
+	}
+
+}
