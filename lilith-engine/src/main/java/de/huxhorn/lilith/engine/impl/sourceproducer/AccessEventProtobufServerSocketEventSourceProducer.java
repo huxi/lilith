d72a6360f4cd6bd0100e5615504473d7694eb3c9
huxhorn <huxhorn@4afeb0b7-f550-0410-824e-ec7672375c7e>
2009-03-15 12:29:14 +0000

    Ongoing work concerning AccesEvents.
    git-svn-id: https://lilith.svn.sourceforge.net/svnroot/lilith/trunk@208 4afeb0b7-f550-0410-824e-ec7672375c7e

---
+++ lilith-engine/src/main/java/de/huxhorn/lilith/engine/impl/sourceproducer/AccessEventProtobufServerSocketEventSourceProducer.java
@@ -0,0 +1,41 @@
+package de.huxhorn.lilith.engine.impl.sourceproducer;
+
+import de.huxhorn.lilith.data.access.AccessEvent;
+import de.huxhorn.lilith.data.eventsource.EventWrapper;
+import de.huxhorn.lilith.data.eventsource.SourceIdentifier;
+import de.huxhorn.lilith.engine.EventProducer;
+import de.huxhorn.lilith.engine.impl.eventproducer.AccessEventProtobufMessageBasedEventProducer;
+import de.huxhorn.sulky.buffers.AppendOperation;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AccessEventProtobufServerSocketEventSourceProducer
+	extends AbstractServerSocketEventSourceProducer<AccessEvent>
+{
+	private boolean compressing;
+
+	public AccessEventProtobufServerSocketEventSourceProducer(int port, boolean compressing)
+		throws IOException
+	{
+		super(port);
+		this.compressing = compressing;
+	}
+
+	public boolean isCompressing()
+	{
+		return compressing;
+	}
+
+	protected EventProducer createProducer(SourceIdentifier id, AppendOperation<EventWrapper<AccessEvent>> eventQueue, InputStream inputStream)
+		throws IOException
+	{
+		return new AccessEventProtobufMessageBasedEventProducer(id, eventQueue, inputStream, compressing);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "AccessEventProtobufServerSocketEventSourceProducer[port=" + getPort() + ", compressing=" + compressing + "]";
+	}
+}
