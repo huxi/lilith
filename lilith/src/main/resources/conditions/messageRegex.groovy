31d5a7c6042a80a247647c1a0f815739159d731c
huxhorn <huxhorn@4afeb0b7-f550-0410-824e-ec7672375c7e>
2009-05-14 07:54:54 +0000

    Added missing contact.xhtml *sigh* and added another example groovy condition that uses a regex to match the message of an event.
    git-svn-id: https://lilith.svn.sourceforge.net/svnroot/lilith/trunk@265 4afeb0b7-f550-0410-824e-ec7672375c7e

---
+++ lilith/src/main/resources/conditions/messageRegex.groovy
@@ -0,0 +1,28 @@
+/**
+ * Returns true if the logging events message matches the regex 
+ * given as searchString.
+ */
+import de.huxhorn.lilith.data.logging.LoggingEvent;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+def event = input?.event;
+
+if(event instanceof LoggingEvent)
+{
+	try
+	{
+		def pattern = Pattern.compile(searchString);
+		def message = event.message.message;
+		if(message)
+		{
+			return message ==~ pattern; // short for pattern.matcher(message).matches();
+		}
+	}
+	catch(PatternSyntaxException ex)
+	{
+		// ignore, returns false
+	}
+}
+return false;
