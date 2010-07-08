0b299e35bcf8382dc55f654c76062a1430b01c33
Joern Huxhorn <huxhorn@users.sourceforge.net>
2010-07-09 00:08:35 +0200

    Commented out class because it isn't currently used anyway.

--- lilith-data/logging/src/main/java/de/huxhorn/lilith/data/logging/ThreadGroupInfo.java
+++ lilith-data/logging/src/main/java/de/huxhorn/lilith/data/logging/ThreadGroupInfo.java
@@ -32,60 +32,60 @@
  * limitations under the License.
  */
 
-package de.huxhorn.lilith.data.logging;
+//package de.huxhorn.lilith.data.logging;
 
-import java.io.Serializable;
 
 /**
  * Not yet used and/or finished.
  */
-public class ThreadGroupInfo
-	implements Serializable, Cloneable
-{
-	private static final long serialVersionUID = -979166102420961284L;
-
-	private String name;
-	private ThreadGroupInfo parent;
-
-	public String getName()
-	{
-		return name;
-	}
-
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-
-	public ThreadGroupInfo getParent()
-	{
-		return parent;
-	}
-
-	public void setParent(ThreadGroupInfo parent)
-	{
-		this.parent = parent;
-	}
-
-	@Override
-	public boolean equals(Object o)
-	{
-		if(this == o) return true;
-		if(o == null || getClass() != o.getClass()) return false;
-
-		ThreadGroupInfo that = (ThreadGroupInfo) o;
-
-		if(name != null ? !name.equals(that.name) : that.name != null) return false;
-		if(parent != null ? !parent.equals(that.parent) : that.parent != null) return false;
-
-		return true;
-	}
-
-	@Override
-	public int hashCode()
-	{
-		int result = name != null ? name.hashCode() : 0;
-		result = 31 * result + (parent != null ? parent.hashCode() : 0);
-		return result;
-	}
-}
+//public class ThreadGroupInfo
+//	implements Serializable, Cloneable
+//{
+//	private static final long serialVersionUID = -979166102420961284L;
+//
+//	private String name;
+//	private ThreadGroupInfo parent;
+//
+//	public String getName()
+//	{
+//		return name;
+//	}
+//
+//	public void setName(String name)
+//	{
+//		this.name = name;
+//	}
+//
+//	public ThreadGroupInfo getParent()
+//	{
+//		return parent;
+//	}
+//
+//	public void setParent(ThreadGroupInfo parent)
+//	{
+//		this.parent = parent;
+//	}
+//
+//	@Override
+//	public boolean equals(Object o)
+//	{
+//		if(this == o) return true;
+//		if(o == null || getClass() != o.getClass()) return false;
+//
+//		ThreadGroupInfo that = (ThreadGroupInfo) o;
+//
+//		if(name != null ? !name.equals(that.name) : that.name != null) return false;
+//		if(parent != null ? !parent.equals(that.parent) : that.parent != null) return false;
+//
+//		return true;
+//	}
+//
+//	@Override
+//	public int hashCode()
+//	{
+//		int result = name != null ? name.hashCode() : 0;
+//		result = 31 * result + (parent != null ? parent.hashCode() : 0);
+//		return result;
+//	}
+//
+//}
