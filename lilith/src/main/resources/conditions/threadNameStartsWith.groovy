80ca1ccc2138e649f4b5560cea62b49789304280
Joern Huxhorn <huxhorn@users.sourceforge.net>
2011-03-29 15:11:15 +0200

    Copyright notice 2010 => 2011

--- lilith/src/main/resources/conditions/threadNameStartsWith.groovy
+++ lilith/src/main/resources/conditions/threadNameStartsWith.groovy
@@ -2,7 +2,7 @@ import de.huxhorn.lilith.data.logging.LoggingEvent
 
 /*
  * Lilith - a log event viewer.
- * Copyright (C) 2007-2010 Joern Huxhorn
+ * Copyright (C) 2007-2011 Joern Huxhorn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
