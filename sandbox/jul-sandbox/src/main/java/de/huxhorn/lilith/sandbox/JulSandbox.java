a1ac8fb065057efe040159ae1914abcd04526e80
Joern Huxhorn <huxhorn@users.sourceforge.net>
2011-07-10 19:51:25 +0200

    Added suppressed to jul-sandbox to validate my assumption that "suppressed" is not supported by the jul xml layout (neither is "cause").
    Also added log.xml and stacktrace.txt for future reference.

--- sandbox/jul-sandbox/src/main/java/de/huxhorn/lilith/sandbox/JulSandbox.java
+++ sandbox/jul-sandbox/src/main/java/de/huxhorn/lilith/sandbox/JulSandbox.java
@@ -2,9 +2,42 @@ package de.huxhorn.lilith.sandbox;
 
 import java.util.logging.*;
 import java.io.*;
+import java.lang.reflect.Method;
 
 public class JulSandbox
 {
+	public static final Method ADD_SUPPRESSED_METHOD;
+
+	static
+	{
+	    Method method = null;
+		try
+		{
+			method = Throwable.class.getMethod("addSuppressed", Throwable.class);
+		}
+		catch(NoSuchMethodException e)
+		{
+			// ignore
+		}
+		ADD_SUPPRESSED_METHOD = method;
+	}
+
+	public static void addSuppressed(Throwable throwable, Throwable suppressed)
+	{
+		if(ADD_SUPPRESSED_METHOD != null)
+		{
+			try
+			{
+				ADD_SUPPRESSED_METHOD.invoke(throwable, suppressed);
+			}
+			catch(Throwable t)
+			{
+				System.err.println("Exception while invoking Throwable.addSuppressed method!");
+				t.printStackTrace();
+			}
+		}
+	}
+
 	public static class InnerClass
 	{
 		public static void execute()
@@ -12,6 +45,12 @@ public class JulSandbox
 			final Logger logger = Logger.getLogger(InnerClass.class.getName());
 			logger.log(Level.INFO,"Foo!");
 			logger.log(Level.WARNING,"Foo!", createRuntimeException());
+			Throwable t=createRuntimeException();
+			addSuppressed(t, new RuntimeException("Suppressed1"));
+			addSuppressed(t, new RuntimeException("Suppressed2"));
+			logger.log(Level.SEVERE,"Foo with Suppressed!", t);
+			System.out.println("printStackTrace:");
+			t.printStackTrace(System.out);
 		}
 	}
 
