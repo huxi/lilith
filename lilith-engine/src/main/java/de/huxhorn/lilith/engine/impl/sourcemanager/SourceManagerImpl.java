d6e6d80a4710ad74a8e059078ded5e0f5c7afbe5
Joern Huxhorn <huxhorn@users.sourceforge.net>
2015-05-13 09:57:46 +0200

    Made sources final.

--- lilith-engine/src/main/java/de/huxhorn/lilith/engine/impl/sourcemanager/SourceManagerImpl.java
+++ lilith-engine/src/main/java/de/huxhorn/lilith/engine/impl/sourcemanager/SourceManagerImpl.java
@@ -1,6 +1,6 @@
 /*
  * Lilith - a log event viewer.
- * Copyright (C) 2007-2011 Joern Huxhorn
+ * Copyright (C) 2007-2015 Joern Huxhorn
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -48,7 +48,7 @@ public class SourceManagerImpl<T extends Serializable>
 	private BlockingCircularBuffer<EventWrapper<T>> queue;
 	private Set<EventSourceListener<T>> listeners;
 	private PropertyChangeSupport changeSupport;
-	private List<EventSource<T>> sources;
+	private final List<EventSource<T>> sources;
 	private final Set<EventProducer<T>> eventProducers;
 	private final Lock eventProducersLock;
 	private EventPoller<T> eventPoller;
