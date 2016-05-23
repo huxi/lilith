de6b919bd16d3a518c3188740c5b6085b15b570f
Joern Huxhorn <huxhorn@users.sourceforge.net>
2016-05-23 15:52:53 +0200

    Another condition result handling type.

--- lilith-conditions/src/test/groovy/de/huxhorn/lilith/conditions/GroovyConditionSpec.groovy
+++ lilith-conditions/src/test/groovy/de/huxhorn/lilith/conditions/GroovyConditionSpec.groovy
@@ -53,6 +53,7 @@ class GroovyConditionSpec extends Specification {
 		'WorkingCondition'              | null          | [67, 97] as Set
 		'WorkingScript'                 | null          | [67, 97] as Set
 		'WorkingScriptReturnOther'      | null          | [67, 97] as Set
+		'WorkingScriptThrowing'         | null          | [67, 97] as Set
 		'WorkingScriptWithSearchString' | null          | [6, 7, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 65, 66, 67, 68, 69, 76, 78, 80, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 115, 116] as Set
 		'WorkingScriptWithSearchString' | 'com.foo.Foo' | [13] as Set
 		'SyntaxError'                   | null          | [] as Set
