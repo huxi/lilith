df571a64c7b41517cf32f4f9df1e9c34102a1a1c
Joern Huxhorn <huxhorn@users.sourceforge.net>
2017-04-10 14:05:13 +0200

    Removed unused constructors.

--- lilith/src/main/java/de/huxhorn/lilith/swing/table/renderer/ConditionalBorder.java
+++ lilith/src/main/java/de/huxhorn/lilith/swing/table/renderer/ConditionalBorder.java
@@ -1,6 +1,6 @@
 /*
  * Lilith - a log event viewer.
- * Copyright (C) 2007-2011 Joern Huxhorn
+ * Copyright (C) 2007-2017 Joern Huxhorn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package de.huxhorn.lilith.swing.table.renderer;
 
 import java.awt.Color;
@@ -35,16 +36,6 @@ public class ConditionalBorder
 	private int thickness;
 	private int innerThickness;
 	private Color borderColor;
-
-	public ConditionalBorder(Color color)
-	{
-		this(color, 1, 0);
-	}
-
-	public ConditionalBorder(Color color, int thickness)
-	{
-		this(color, thickness, 0);
-	}
 
 	public ConditionalBorder(Color color, int thickness, int innerThickness)
 	{
