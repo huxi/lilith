0b2c54b04e83456a06078f0e5ebf6942cf6a32cf
Joern Huxhorn <huxhorn@users.sourceforge.net>
2017-07-27 14:01:31 +0200

    Initialize sourceIdentifierUpdater.

--- lilith-engine/src/main/java/de/huxhorn/lilith/engine/impl/sourceproducer/ConvertingServerSocketEventSourceProducer.java
+++ lilith-engine/src/main/java/de/huxhorn/lilith/engine/impl/sourceproducer/ConvertingServerSocketEventSourceProducer.java
@@ -1,6 +1,6 @@
 /*
  * Lilith - a log event viewer.
- * Copyright (C) 2007-2013 Joern Huxhorn
+ * Copyright (C) 2007-2017 Joern Huxhorn
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,7 +17,7 @@
  */
 
 /*
- * Copyright 2007-2013 Joern Huxhorn
+ * Copyright 2007-2017 Joern Huxhorn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,8 +55,8 @@ public class ConvertingServerSocketEventSourceProducer<T extends Serializable>
 			throws IOException
 	{
 		super(port);
-
 		setConverterRegistry(converterRegistry);
+		setSourceIdentifierUpdater(sourceIdentifierUpdater);
 	}
 
 	public ConverterRegistry<T> getConverterRegistry()
